821589914d524a67ccc2d02de1e2dd118fd6ad0b
David calvin <chumacalvin8@gmail.com>
2025-02-09 14:17:08 +0300

    changed registration.httml to register.httml and configured it correctly

--- Pilot_Logbook/users/views.py
+++ Pilot_Logbook/users/views.py
@@ -1,11 +1,51 @@
-from django.shortcuts import render,redirect
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'others/home.html', {})
 
 def register(request):
-    return render(request, 'registration/registration.html', {})
+    form = CreateUserForm()
+    context = {'form': form}
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)  
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,"Account was created for " + user)
+                
+                return redirect('login')  # ✅ Redirect after successful registration
+            else:
+                messages.error(request, "There was an error with your registration. Please check the details below.")
+        else:
+            form = CreateUserForm()
+        return render(request, 'registration/register.html', context)  # ✅ Added return statement for GET requests
+
 
 def login_view(request):
-    return render(request, 'registration/login.html', {})
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method== 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+
+            user = authenticate(request, username= username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,"username or password is incorrect")
+        return render(request, 'registration/login.html', {})
+    
+def logout_view(request):
+    logout(request)
+    return redirect('register')
