88eb8692f5c6f7b32401a14e7ef355ee147b47ef
David calvin <chumacalvin8@gmail.com>
2025-02-09 10:37:36 +0300

    Configured urls succesful one can move from one page to another

--- Pilot_Logbook/users/views.py
+++ Pilot_Logbook/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'others/home.html', {})
+
+def register(request):
+    return render(request, 'registration/registration.html', {})
+
+def login_view(request):
+    return render(request, 'registration/login.html', {})
