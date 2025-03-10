1a236c8e1a60912ab5450370aad14ffbde3a22e8
Calvin <chumacalvin8@gmail.com>
2025-03-11 00:24:21 +0300

    Add search functionality to flight logs and improve log list template solves: Implement Search Functionality for Logs #13

--- Pilot_Logbook/logs/views.py
+++ Pilot_Logbook/logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Log
 from .forms import LogForm
+from django.db.models import Q
 
 @login_required
 def create_log(request):
@@ -21,17 +22,28 @@ def create_log(request):
 
 @login_required
 def log_list(request):
-    logs = Log.objects.filter(user=request.user).order_by('-date')  # Show only user's logs
+    logs = Log.objects.filter(user=request.user).order_by('-date')  
+    query = request.GET.get('query')  
+    if query:
+        logs = logs.filter(
+            Q(aircraft__icontains=query) |
+            Q(aircraft_type__icontains=query) |
+            Q(aircraft_registration__icontains=query) |
+            Q(departure_airport__icontains=query) |
+            Q(arrival_airport__icontains=query) |
+            Q(flight_number__icontains=query) |
+            Q(remarks__icontains=query)
+        )
     return render(request, 'logs/log_list.html', {'logs': logs})
 
 @login_required
 def edit_log(request, log_id):
-    log = get_object_or_404(Log, id=log_id, user=request.user)  # Ensure user can only edit their own logs
+    log = get_object_or_404(Log, id=log_id, user=request.user)  #Ensure user can only edit their own logs
     
     if request.method == "POST":
         log_form = LogForm(request.POST, instance=log)
         if log_form.is_valid():
-            log_form.save()  # No need to manually assign user again
+            log_form.save()  
             return redirect('logs:logs_list')
     else:
         log_form = LogForm(instance=log)
